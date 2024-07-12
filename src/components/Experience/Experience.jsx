0b0d0e712523950c8aefbc68274ba3fe21df1cbe
Med95B <bouizloufa@gmail.com>
2024-07-12 18:28:25 +0100

    experience dark mode

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,21 +1,27 @@
 import './Experience.css'
+import { ThemeContext } from '../../Context'
+import { useContext } from 'react'
 
 function Experience() {
+
+  const theme=useContext(ThemeContext)
+  const darkMode=theme.state.darkMode
+
   return (
     <div className='experience' id='Experience'>
         <div className="achievement">
-        <div className="circle">8+</div>
-        <span>years</span>
+        <div className="circle" style={{border:darkMode?'6px solid orange':''}}>8+</div>
+        <span style={{color:darkMode?'white':''}}>years</span>
         <span>Experience</span>
         </div>
         <div className="achievement">
-        <div className="circle">20+</div>
-        <span>completed</span>
+        <div className="circle" style={{border:darkMode?'6px solid orange':''}}>20+</div>
+        <span style={{color:darkMode?'white':''}}>completed</span>
         <span>Projects</span>
         </div>
         <div className="achievement">
-        <div className="circle">5+</div>
-        <span>companies</span>
+        <div className="circle" style={{border:darkMode?'6px solid orange':''}}>5+</div>
+        <span style={{color:darkMode?'white':''}}>companies</span>
         <span>work</span>
         </div>
     </div>
